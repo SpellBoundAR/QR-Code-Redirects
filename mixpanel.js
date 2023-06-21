a1fe8d83275ca53d2d0825de2f7e7f3f5fc3e4fe
Chris Monteferrante <55144006+chris-spellbound@users.noreply.github.com>
2023-06-21 16:29:35 -0400

    Update mixpanel.js

--- mixpanel.js
+++ mixpanel.js
@@ -1,1 +1,14 @@
+const projectToken = '88be7bfc5efced7c0f631f9328d54fc2';
 
+function sendQRScanEvent() 
+{
+  mixpanel.init(projectToken, { debug: true, track_pageview: false });
+  
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  
+  mixpanel.track('QR Scan', {
+    'Code': urlParams.get('code'),
+    'URL Path': window.location.pathname
+  })
+}
