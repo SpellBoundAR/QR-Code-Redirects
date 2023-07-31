7ad5a70fdfebc11f02d9d35503748c52cf388035
Chris Monteferrante <55144006+chris-spellbound@users.noreply.github.com>
2023-07-31 09:41:26 -0500

    Update mixpanel.js

--- mixpanel.js
+++ mixpanel.js
@@ -1,15 +1,19 @@
 const projectToken = '88be7bfc5efced7c0f631f9328d54fc2';
+const debugToken = '88be7bfc5efced7c0f631f9328d54fc2';
 
 function sendQRScanEvent() 
 {
-  mixpanel.init(projectToken, { debug: false, track_pageview: false });
-  
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
+
+  let debug = urlParams.has('debug') && urlParams.get('debug') === true;
+  console.log("DEBUG: " + debug);
+  if (debug) mixpanel.init(debugToken, { debug: true, track_pageview: false });
+  else mixpanel.init(projectToken, { debug: false, track_pageview: false });
   
-  mixpanel.track('QR Scan', {
+  /*mixpanel.track('QR Scan', {
     'Code': urlParams.has('code') ? urlParams.get('code') : "none",
     'Variant': urlParams.has('variant') ? urlParams.get('variant') : "default",
     'URL Path': window.location.pathname
-  })
+  })*/
 }
