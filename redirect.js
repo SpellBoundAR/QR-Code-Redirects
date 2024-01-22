07d648fdf4e2b38d9ce2f5d1ce0c476d805f7f55
Chris Monteferrante <55144006+chris-spellbound@users.noreply.github.com>
2024-01-22 09:55:37 -0700

    Added Vibe ARcade

--- redirect.js
+++ redirect.js
@@ -20,3 +20,26 @@ function redirectToSpellBoundAppStore()
   // Base case
   else window.location.href = "https://apps.apple.com/us/app/spellbound-ar/id1033984853";
 }
+
+function redirectToVibeARcadeAppStore()
+{
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  
+  // Windows Phone must come first because its UA also contains "Android"
+  if (/windows phone/i.test(userAgent)) {
+    window.location.href = "https://play.google.com/store/apps/details?id=com.spellbound.vibearcade";
+  }
+  
+  // Android
+  else if (/android/i.test(userAgent)) {
+    window.location.href = "https://play.google.com/store/apps/details?id=com.spellbound.vibearcade";
+  }
+  
+  // iOS detection from: http://stackoverflow.com/a/9039885/177710
+  else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    window.location.href = "https://apps.apple.com/nz/app/vibe-arcade/id6475691258";
+  }
+  
+  // Base case
+  else window.location.href = "https://apps.apple.com/nz/app/vibe-arcade/id6475691258";
+}
